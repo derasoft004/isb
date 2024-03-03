cff0260d1d21798ef6864c154a5caf5e36e9bb8d
Egor <derasoft004@gmail.com>
2024-03-03 18:02:31 +0400

    third commit. get equals_dict witch varies depending on the hash-table in memory (I need to change the "equal smbls" order)

--- lab_1/main.py
+++ lab_1/main.py
@@ -48,12 +48,12 @@ def task_1(file_to_read: str, file_to_write: str) -> None:
     write_file(file_to_write, rot16(read_file(file_to_read)))
 
 
-def global_symbols_frequency():
+def global_symbols_frequency() -> dict:
     """
 
     :return:
     """
-    frequency_lst = read_file('task_2/frequency_symbols_based.txt').split('\n')
+    frequency_lst = [i.replace('§', ' ') for i in read_file('task_2/frequency_symbols_based.txt').split('\n')]
     dictionary = dict()
     symbols, frequency = \
         [frequency_lst[i][0] for i in range(len(frequency_lst))], \
@@ -70,27 +70,61 @@ def encrypted_text_symbols_frequency() -> dict:
 
     :return: text's symbols dictionary
     """
-    encrypted_text = read_file('task_2/encrypted_text.txt')
+    encrypted_text = read_file('task_2/cod8.txt')
     symbols_dict = dict()
     for key in set(encrypted_text):
         symbols_dict[key] = float(round(encrypted_text.count(key) / len(encrypted_text), 6))
     return symbols_dict
 
 
-def task_2():
+def get_equals_dict() -> dict:
     gsf = global_symbols_frequency()
     etsf = encrypted_text_symbols_frequency()
 
-    s_symbols_frequency = sorted(global_symbols_frequency().values(), reverse=True)
-    s_encrypted_text_frequency = sorted(encrypted_text_symbols_frequency().values(), reverse=True)
+    s_encrypted_text_frequency = sorted(etsf.values(), reverse=True)
 
-    rep_list = dict()
+    sorted_text_dict = dict()
+    for frequency in s_encrypted_text_frequency:
+        for etsf_key in etsf.keys():
+            if etsf[etsf_key] == frequency:
+                sorted_text_dict[etsf_key] = frequency
+    # print(gsf, '\n\n', sorted_text_dict, '\n\n', )
+
+    sorted_text_dict.pop('-')
+    equals_dict = dict()
+    i = 0
+    for etsf_key in sorted_text_dict.keys():
+        j = 0
+        for gsf_key in gsf.keys():
+            j += 1
+            equals_dict[etsf_key] = gsf_key
+            if j > i: break
+
+        i += 1
+
+    print(gsf, '\n\n', sorted_text_dict, '\n\n', equals_dict)
+
+    return equals_dict
+
+
+def task_2():
+    encrypted_text = read_file('task_2/cod8.txt')
+    ret_str = ''
+    symbols = get_equals_dict()
+    for symbol in encrypted_text:
+        try:
+            ret_str += symbols[symbol]
+        except KeyError:
+            ret_str += symbol
+    print(ret_str)
+
     # TODO
-
+    #  генерируются расшифрованные варианты, но они 99/100 не верные (число приведено для наглядности), тк в памяти
+    #  в хеш таблице sorted_text_dict элементы расположены рандомно
 
 
 def main() -> None:
-    # task_1('task_1/common_text.txt', 'task_1/encrypted_text.txt')
+    # task_1('task_1/common_text.txt', 'task_1/cod8.txt')
     task_2()
 
 
