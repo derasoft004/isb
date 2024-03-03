1b4bad32b7d100fe7ed0ea60f9fe53eac6cd1005
Egor <derasoft004@gmail.com>
2024-03-03 20:29:10 +0400

    4th commit. ready key (as dictionary and as .json file) and task two

--- lab_1/main.py
+++ lab_1/main.py
@@ -1,6 +1,7 @@
 """
 это самое
 """
+import json
 
 RU_ALPH = 'АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
 
@@ -50,6 +51,7 @@ def task_1(file_to_read: str, file_to_write: str) -> None:
 
 def global_symbols_frequency() -> dict:
     """
+    count stat for symbols in global using
 
     :return:
     """
@@ -77,50 +79,60 @@ def encrypted_text_symbols_frequency() -> dict:
     return symbols_dict
 
 
-def get_equals_dict() -> dict:
-    gsf = global_symbols_frequency()
-    etsf = encrypted_text_symbols_frequency()
-
-    s_encrypted_text_frequency = sorted(etsf.values(), reverse=True)
-
-    sorted_text_dict = dict()
-    for frequency in s_encrypted_text_frequency:
-        for etsf_key in etsf.keys():
-            if etsf[etsf_key] == frequency:
-                sorted_text_dict[etsf_key] = frequency
-    # print(gsf, '\n\n', sorted_text_dict, '\n\n', )
-
-    sorted_text_dict.pop('-')
-    equals_dict = dict()
-    i = 0
-    for etsf_key in sorted_text_dict.keys():
-        j = 0
-        for gsf_key in gsf.keys():
-            j += 1
-            equals_dict[etsf_key] = gsf_key
-            if j > i: break
-
-        i += 1
-
-    print(gsf, '\n\n', sorted_text_dict, '\n\n', equals_dict)
-
-    return equals_dict
+# def get_equals_dict() -> None:
+#     """
+#     function used for keys permutations
+#
+#     :return:
+#     """
+#     gsf = global_symbols_frequency()
+#     etsf = encrypted_text_symbols_frequency()
+#
+#     s_encrypted_text_frequency = sorted(etsf.values(), reverse=True)
+#
+#     sorted_text_dict = dict()
+#     for frequency in s_encrypted_text_frequency:
+#         for etsf_key in etsf.keys():
+#             if etsf[etsf_key] == frequency:
+#                 sorted_text_dict[etsf_key] = frequency
+#
+#     cor_key = [' ', 'Е', 'А', 'О', 'И', 'Н', 'Т', 'Р', 'С', 'В', 'М', 'П', 'Л', 'Д', 'Я', 'Ы', 'Х', 'Ь', 'К', 'Ч', 'Ф',
+#                'У', 'З', 'Ж', 'Г', 'Ю', 'Б', 'Й', 'Ш', 'Э', 'Ц', 'Ъ', 'Щ']
+#     """
+#     набор данных у которых точность некоторых символов одинаковая
+#     (что означает равновозможную вероятность появления некоторого одного символа вместо некоторого другого)
+#     """
+#
+#     equals_dict = dict()
+#     i = 0
+#     for etsf_key in sorted_text_dict.keys():
+#         j = 0
+#         for gsf_key in cor_key:
+#             j += 1
+#             equals_dict[etsf_key] = gsf_key
+#             if j > i: break
+#
+#         i += 1
+#     print(dict(zip(sorted_text_dict.keys(), cor_key)))
+#
 
 
 def task_2():
     encrypted_text = read_file('task_2/cod8.txt')
     ret_str = ''
-    symbols = get_equals_dict()
+    # key = {'Я': ' ', 'Д': 'Е', ' ': 'А', '4': 'О', 't': 'И', 'М': 'Н', 'Р': 'Т', 'О': 'Р', 'П': 'С', 'Б': 'В', 'Л': 'М',
+    #        'К': 'Л', '5': 'П', '2': 'Д', 'Ь': 'Я', 'Щ': 'Ы', 'У': 'Х', 'Ъ': 'Ь', 'Й': 'К', 'r': 'З', 'Х': 'Ч', '>': 'У',
+    #        'Т': 'Ф', 'Е': 'Ж', '1': 'Г', 'Ы': 'Ю', 'А': 'Б', 'И': 'Й', 'Ц': 'Ш', '?': 'Ъ', 'Ф': 'Ц', 'Ч': 'Щ', 'w': 'Э'}
+
+    with open('task_2/key.json') as f:
+        key = json.load(f)
+
     for symbol in encrypted_text:
         try:
-            ret_str += symbols[symbol]
+            ret_str += key[symbol]
         except KeyError:
             ret_str += symbol
     print(ret_str)
-
-    # TODO
-    #  генерируются расшифрованные варианты, но они 99/100 не верные (число приведено для наглядности), тк в памяти
-    #  в хеш таблице sorted_text_dict элементы расположены рандомно
 
 
 def main() -> None:
