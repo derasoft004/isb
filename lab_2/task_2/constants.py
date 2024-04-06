db83345f0acba9de79c1d0b0a6e0792ff2989ba7
Egor <derasoft004@gmail.com>
2024-04-06 22:11:41 +0400

    1-th after-finally commit fixed imports and few functions

--- lab_2/task_2/constants.py
+++ lab_2/task_2/constants.py
@@ -1,10 +1,13 @@
 from functions_assistant import load_json
 
 PATHS = 'paths.json'
+CPP_P = 'cpp_png'
+JAVA_P = 'java_png'
+
 N = 128
 M = 8
 P0, P1, P2, P3 = 0.2148, 0.3672, 0.2305, 0.1875
 
 paths_dictionary = load_json(PATHS)
-PATH_CPP_PNG = paths_dictionary['cpp_png']
-PATH_JAVA_PNG = paths_dictionary['java_png']
+PATH_CPP_PNG = paths_dictionary[CPP_P]
+PATH_JAVA_PNG = paths_dictionary[JAVA_P]
--- lab_2/task_2/functions_assistant.py
+++ lab_2/task_2/functions_assistant.py
@@ -37,24 +37,15 @@ def substring_in_sentence(sequence: str) -> int:
     :param sequence: bit sequence
     :return: max length of substring
     """
-    counter_dict = dict((str(i), 0) for i in range(1, 5))
-    count, max_len = 0, 1
-    sequence_not_null = False
+    max_len, cur_len = 0, 0
 
-    def adder(c):
-        if c in range(1, 5):
-            counter_dict[str(c)] += 1
-        elif c >= 5:
-            counter_dict['4'] += 1
-
-    for ind in range(len(sequence)):
-        if int(sequence[ind]):
-            sequence_not_null = True
-            count += 1
+    for symbol in sequence:
+        if symbol == '1':
+            cur_len += 1
         else:
-            adder(count)
-            count = 0
-    if sequence_not_null:
-        adder(count)
-        max_len = max(int(key) for key, value in counter_dict.items() if value)
-    return max_len
+            max_len = max(max_len, cur_len)
+            cur_len = 0
+
+    max_len = max(max_len, cur_len)
+
+    return max_len if max_len < 4 else 4
