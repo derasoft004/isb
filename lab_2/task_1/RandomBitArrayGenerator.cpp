c439db38a777fb8a255dfabea938ac0406d32aa7
Egor <derasoft004@gmail.com>
2024-04-06 22:21:11 +0400

    1-th after-finally commit fixed imports and few functions, added docstrings into cpp and java files

--- lab_2/task_1/RandomBitArrayGenerator.cpp
+++ lab_2/task_1/RandomBitArrayGenerator.cpp
@@ -2,21 +2,35 @@
 #include <cstdlib>
 #include <ctime>
 
+
 void generateRandomBitArray(int bitArray[], int size) {
-    srand(time(nullptr)); // Устанавливаем начальное значение для генератора случайных чисел
+    /**
+     * Generates a random bit array of a given size.
+     *
+     * @param bitArray bit array to fill with random values.
+     * @param size size of the bit array.
+    */
+    srand(time(nullptr)); // Set the seed for the random number generator
 
     for (int i = 0; i < size; i++) {
-        bitArray[i] = rand() % 2; // Генерируем случайное число 0 или 1
+        bitArray[i] = rand() % 2; // Generate a random number 0 or 1
     }
 }
 
+
 int main() {
+    /**
+     * Demonstrates the use of the generateRandomBitArray function to generate a random bit array of
+     * length 128 and prints it to the console.
+     *
+     * @return 0 on success.
+     */
     const int SIZE = 128;
     int randomBitArray[SIZE];
 
     generateRandomBitArray(randomBitArray, SIZE);
 
-    // Выводим сгенерированный массив на экран
+    // Print the generated bit array to the console
     for (int i = 0; i < SIZE; i++) {
         std::cout << randomBitArray[i];
     }
--- lab_2/task_1/RandomBitArrayGenerator.java
+++ lab_2/task_1/RandomBitArrayGenerator.java
@@ -1,23 +1,33 @@
-import java.util.Random;
-
 public class RandomBitArrayGenerator {
 
+    /**
+     * Generates a random bit array of a given size.
+     *
+     * @param size size of the bit array.
+     * @return random bit array.
+     */
+    public static int[] generateRandomBitArray(int size) {
+        Random random = new Random();
+        int[] bitArray = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            bitArray[i] = random.nextInt(2); // Generate a random number 0 or 1
+        }
+
+        return bitArray;
+    }
+
+    /**
+     * Demonstrates the use of the generateRandomBitArray(int size)
+     * method to generate a random bit array of length 128 and prints it to the console.
+     *
+     * @param args command line arguments.
+     */
     public static void main(String[] args) {
         int[] randomBitArray = generateRandomBitArray(128);
 
         for (int i = 0; i < randomBitArray.length; i++) {
             System.out.print(randomBitArray[i]);
         }
-    }
-
-    public static int[] generateRandomBitArray(int size) {
-        Random random = new Random();
-        int[] bitArray = new int[size];
-
-        for (int i = 0; i < size; i++) {
-            bitArray[i] = random.nextInt(2); // Генерируем случайное число 0 или 1
-        }
-
-        return bitArray;
     }
 }
